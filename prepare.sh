bee630cfdf35ac7808c146563c61f9bf1677bf4d
Marialina <nina.ball@gmail.com>
2016-04-08 13:45:27 -0400

    Create prepare.sh

---
+++ prepare.sh
@@ -0,0 +1,4 @@
+ #! /bin/bash
+apt-get install apt-transport-https ca-certificates
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo 'deb https://apt.dockerproject.org/repo debian-jessie main' > /etc/apt/sources.list.d/docker.list
